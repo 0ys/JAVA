14adc722bf6b445bfb93a23a05e3aba32577e625
0ys <ysgong@yonsei.ac.kr>
2023-08-13 16:47:50 +0900

    클래스와 객체

--- BasicOOP/src/staticex/singleton/CarFactoryTest.java
+++ BasicOOP/src/staticex/singleton/CarFactoryTest.java
@@ -1,13 +1,2 @@
-package staticex.singleton;
-
-public class CarFactoryTest {
-    public static void main(String[] args) {
-        CarFactory factory = CarFactory.getInstance();
-        Car mySonata = factory.createCar();
-        Car yourSonata = factory.createCar();
-
-        System.out.println(mySonata.getCarNum());
-        System.out.println(yourSonata.getCarNum());
-
-    }
+package staticex.singleton;public class CarFactoryTest {
 }
